c85dc58af79b394f94e89ac6fe7072494504f1bc
freh-g <francesco.gualdi01@estudiant.upf.edu>
2022-11-30 12:02:40 +0000

    updated setup for pubishing on pypi

--- setup.py
+++ setup.py
@@ -5,17 +5,10 @@ from codecs import open
 from os import path
 
 
-# The directory containing this file
-HERE = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name='papysql',
-    
-    version='0.1.0',
+    packages=find_packages(),
+    version='0.2.2222222222222222222222',
     long_description='This is a library to interface SQL with python. The basic library read the tables with list_tables method and read them with display_table. When a table is red is then converted into a pandas DataFrame. Moreover it gives the possibility to read a SQL script that builds a db with ExecuteScriptFromFile',
     author='Francesco Gualdi',
     license='MIT'
