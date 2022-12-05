bdd9324fa19715d0b9132c15139fdac1e232c4a3
freh-g <francesco.gualdi01@estudiant.upf.edu>
2022-12-05 10:54:10 +0100

    Modified setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ from os import path
 setup(
     name='papysql',
     packages=find_packages(),
-    version='0.2.2222222222222222222222',
+    version='0.2.3',
     long_description='This is a library to interface SQL with python. The basic library read the tables with list_tables method and read them with display_table. When a table is red is then converted into a pandas DataFrame. Moreover it gives the possibility to read a SQL script that builds a db with ExecuteScriptFromFile',
     author='Francesco Gualdi',
-    license='MIT'
+    license='GPL'
 )
